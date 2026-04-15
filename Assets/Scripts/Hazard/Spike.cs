66a64764b36ded4006514eff2807fdd56d20f383
morpheus-ble <morpheusfloydmffmffmff@gmail.com>
2026-04-16 04:18:59 +0800

    updated spikes

--- Assets/Scripts/Hazard/Spike.cs
+++ Assets/Scripts/Hazard/Spike.cs
@@ -2,11 +2,31 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour, IDamageDealer
 {
-    [SerializeField] int spikeDamage;
-    [SerializeField] Player thisPlayer;
+    [SerializeField] private int spikeDamage;
+    [SerializeField] private Player thisPlayer;
+
     public int Damage => spikeDamage;
+
+    private void Awake()
+    {
+        if (thisPlayer == null)
+        {
+            thisPlayer = FindObjectOfType<Player>();
+            if (thisPlayer == null)
+                Debug.LogWarning($"Spike.Awake: Player not found in scene for '{gameObject.name}'.");
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        thisPlayer.Respawn();
+        var collidingPlayer = collision.GetComponent<Player>() ?? collision.GetComponentInParent<Player>();
+        if (collidingPlayer != null)
+        {
+            collidingPlayer.Respawn();
+            return;
+        }
+
+        if (thisPlayer != null)
+            thisPlayer.Respawn();
     }
 }
