98f979d395fa4e81d04057a3999192b4dd61042b
rayversalazar <rayversalazar01@gmail.com>
2026-04-18 01:14:29 +0800

    Add new player sprites, animations & prefabs
    Add a large set of new player sprite assets and metadata under Assets/Sprites/New Player. Rename Attack.anim to Attack1.anim and add a new Attack2.anim (updated sample rates and event timings). Update existing animations (Dash, Idle, Walk) to reference the new sprites, and update Player.controller and Player.prefab accordingly. Add PlayerOld.prefab and a new scene asset. Also modify player state scripts (AttackState.cs, FallingState.cs, JumpState.cs) to work with the animation changes.

--- Assets/Scripts/Player/AttackState.cs
+++ Assets/Scripts/Player/AttackState.cs
@@ -2,60 +2,65 @@ using UnityEngine;
 
 public class AttackState : BaseState
 {
-    bool isAttacking;
+    bool attackFinished = false;
+    bool attackSwitch;
     [SerializeField] float attackSpeed = 1f;
     [SerializeField] float defaultAnimationSpeed;
-    string animParameterName = "Attack";
-
+    string attackAnimation1 = "Attack1";
+    string attackAnimation2 = "Attack2";
+    int attack1parameter;
+    int attack2parameter;
     int animParameterId;
-    AnimatorStateInfo animationState;
     public override void Initialize()
     {
-        base.Initialize(); 
-        animParameterId = Animator.StringToHash(animParameterName);
-        animationState = baseAnimator.GetCurrentAnimatorStateInfo(animParameterId);
-       
+        base.Initialize();
+        attack1parameter = Animator.StringToHash(attackAnimation1);
+        attack2parameter = Animator.StringToHash(attackAnimation2);
+        animParameterId = attack1parameter;
     }
 
     public override void OnEnter(PlayerStateMachine state)
     {
         base.OnEnter(state);
-        baseAnimator.SetBool(animParameterId, true);
-        isAttacking = true;
         defaultAnimationSpeed = baseAnimator.speed;
-        baseAnimator.speed = attackSpeed;
-        baseSoundFX.Play(baseSoundFX.attack);
+        baseAnimator.SetBool(animParameterId, true);
     }
 
     public void StartAttack()
-    {
+    { 
+        baseAnimator.speed = attackSpeed;
         baseHitbox.layer = LayerMask.NameToLayer("Player Hit Box");
         basePhysics.rigidbody.linearVelocity = Vector2.zero;
     }
     public void EndAttack()
     {
-        isAttacking = false;
         baseHitbox.layer = 0;
-        baseAnimator.speed = defaultAnimationSpeed;
+        attackFinished = true;
     }
 
     public override void OnExit(PlayerStateMachine state)
     {
         baseAnimator.SetBool(animParameterId, false);
+        attackSwitch = !attackSwitch;
+        animParameterId = attackSwitch == true ? attack1parameter : attack2parameter;
         baseHitbox.layer = 0;
         baseAnimator.speed = defaultAnimationSpeed;
+        attackFinished = false;
     }
 
 
     public override void ProcessAbility(PlayerStateMachine state)
     {
-        if (!isAttacking)
+        if (attackFinished && basePhysics.isGrounded())
         {
             state.ChangeState(state.idle);
-        }
-        if (state.jump.currentAdditionalJumpCount > 0 && baseInputControls.jump.triggered)
+        } else 
+        if (attackFinished &&!basePhysics.isGrounded())
         {
-            state.jump.currentAdditionalJumpCount--;
+            state.ChangeState(state.falling);
+        }
+        if (state.jump.currentJumpCount > 0 && baseInputControls.jump.triggered)
+        {
             state.ChangeState(state.jump);
         }
     }
--- Assets/Scripts/Player/FallingState.cs
+++ Assets/Scripts/Player/FallingState.cs
@@ -1,20 +1,22 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FallingState : BaseState
 {
     [SerializeField] float airMovementSpeed;
-    [SerializeField] float fallSpeed = 1f ;
+    [SerializeField] float gravityMultiplier = 1f ;
+    [SerializeField] float peakJumpTime;
     [SerializeField] float coyoteTimeJump;
-
+    float defaultGravity;
+    bool gravitySwitch;
     public bool AfterRiseState = false;//fix for double jump in coyote time
     string FallingParameterName = "Falling";
     int FallingParameterID;
-    string landingParameterName = "Land";
-    int landingParameterID;
     float time;
+    float currentPeakJumpTime;
     public override void FixedProcessAbility(PlayerStateMachine state)
     {
-        basePhysics.rigidbody.linearVelocity = new Vector2(airMovementSpeed * baseInputControls.horizontalInput, basePhysics.rigidbody.linearVelocityY+-fallSpeed);
+        basePhysics.rigidbody.linearVelocity = new Vector2(airMovementSpeed * baseInputControls.horizontalInput, basePhysics.rigidbody.linearVelocityY);
         player.FlipCharacter();
     }
 
@@ -22,7 +24,8 @@ public class FallingState : BaseState
     {
         base.Initialize();
         FallingParameterID = Animator.StringToHash(FallingParameterName);
-        landingParameterID = Animator.StringToHash(landingParameterName);
+        defaultGravity = basePhysics.rigidbody.gravityScale;
+
     }
 
     public override void OnEnter(PlayerStateMachine state)
@@ -30,20 +33,31 @@ public class FallingState : BaseState
         time = coyoteTimeJump;
         base.OnEnter(state);
         baseAnimator.SetBool(FallingParameterID, true);
+        
+        currentPeakJumpTime = peakJumpTime;
     }
 
     public override void OnExit(PlayerStateMachine state)
     {
-        baseAnimator.SetTrigger(landingParameterID);
         baseAnimator.SetBool(FallingParameterID, false);
         AfterRiseState = false;
-       
+        basePhysics.rigidbody.gravityScale = defaultGravity;
+
     }
 
     public override void ProcessAbility(PlayerStateMachine state)
     {
         time -= Time.deltaTime;
-        if (time>0 && baseInputControls.jump.triggered && !AfterRiseState)
+        
+        if (currentPeakJumpTime > 0)
+        {
+            currentPeakJumpTime -= Time.deltaTime;
+        } 
+        if (currentPeakJumpTime <=0)
+        {
+            basePhysics.rigidbody.gravityScale = defaultGravity * gravityMultiplier;
+        }
+        if (time > 0 && baseInputControls.jump.triggered && !AfterRiseState)
         {
             state.ChangeState(state.jump);
         }
@@ -57,9 +71,8 @@ public class FallingState : BaseState
             state.jump.JumpReset();
             state.ChangeState(state.walk);
         }
-        if (state.jump.currentAdditionalJumpCount > 0 && baseInputControls.jump.triggered && time<=0)
+        if (state.jump.currentJumpCount > 0 && baseInputControls.jump.triggered && time<=0)
         {
-            state.jump.currentAdditionalJumpCount--;
             state.ChangeState(state.jump);
         }
         if (basePhysics.isWallDetected() && baseInputControls.horizontalInput != 0 && !basePhysics.isGrounded())
