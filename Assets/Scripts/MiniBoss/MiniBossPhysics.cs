8c83545a52e7da03ee876efe09d47e3aee0016f5
haez <abalosalmeuz@gmail.com>
2026-04-17 09:14:11 +0800

    graveyard to camp
    made some stuff

--- Assets/Scripts/MiniBoss/MiniBossPhysics.cs
+++ Assets/Scripts/MiniBoss/MiniBossPhysics.cs
@@ -3,33 +3,41 @@ using UnityEngine;
 public class MiniBossPhysics : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
-    [SerializeField] Transform chaseRange;
+    [SerializeField] GameObject chaseRange;
     [SerializeField] float chaseRangeRadius;
-    [SerializeField] Transform attackRange;
+    [SerializeField] GameObject attackRange;
     [SerializeField] float attackRangeRadius;
     [SerializeField] LayerMask target;
     [SerializeField] Transform playerPosition;
 
     public bool PlayerDetectionZone()
     {
-        return Physics2D.OverlapCircle(chaseRange.position, chaseRangeRadius, target);
+        Collider2D detection = Physics2D.OverlapCircle(chaseRange.transform.position, chaseRangeRadius, target);
+        if (detection != null)
+        {
+            playerPosition = detection.transform;
+            return true;
+        }
+        return false;
     }
     public float PlayerPositionX()
     {
-        return playerPosition.position.x > transform.position.x ? 1f : -1f;
+        if (PlayerDetectionZone())
+            return playerPosition.position.x > transform.position.x ? 1f : -1f;
+        return 0;
     }
     private void OnDrawGizmos()
     {
         //for detection
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(chaseRange.position, chaseRangeRadius);
+        Gizmos.DrawWireSphere(chaseRange.transform.position, chaseRangeRadius);
         //for attack
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackRange.position, attackRangeRadius);
+        Gizmos.DrawWireSphere(attackRange.transform.position, attackRangeRadius);
     }
 
     public bool PlayerAttackZoneDetection()
     {
-        return Physics2D.OverlapCircle(attackRange.position, attackRangeRadius, target);
+        return Physics2D.OverlapCircle(attackRange.transform.position, attackRangeRadius, target);
     }
 }
