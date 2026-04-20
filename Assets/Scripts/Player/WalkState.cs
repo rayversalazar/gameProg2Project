5419ad9ad5b8800a1735279b6fb52021ba5d539b
morpheus-ble <morpheusfloydmffmffmff@gmail.com>
2026-04-21 05:57:38 +0800

    my last push on scenes

--- Assets/Scripts/Player/WalkState.cs
+++ Assets/Scripts/Player/WalkState.cs
@@ -6,6 +6,7 @@ public class WalkState : BaseState
     private string walkParameterName = "Walk";
     private int walkParameterID;
 
+
     public override void FixedProcessAbility(PlayerStateMachine state)
     {
         Walk();
